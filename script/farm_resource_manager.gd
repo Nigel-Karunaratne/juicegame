27259fe0002cc849368886a4e4fe47949243fa2d
Nigel Karunaratne <nigel.karunaratne36@gmail.com>
2025-09-03 18:51:27 -0400

    checkpoint

--- script/farm_resource_manager.gd
+++ script/farm_resource_manager.gd
@@ -1,15 +1,41 @@
+"""
+FarmResourceManager
+
+This is a Node that is responsible for managing farming mechanics, like:
+	- C02 and Humidity
+	- Planted Crops
+	- What tiles are open for planting
+
+Node should belong to a group for function access by other scripts. Specifically, the greenhouse UI
+can get a reference to this node and call functions.
+
+Receives and emits signals from the GlobalEventBus.
+"""
+
 class_name FarmResourceManager
 extends Node
 
 var co2_level = 100 #range 100 to 0
 var humidity_level : int = 50 #range 100 to 0
 
+var unlocked_tiles = 1 #range 1-9
+
+# TODO - farming things
+var planted_crops: Array = []
+
 func _ready() -> void:
 	GlobalEventBus.sg_worldtime_hourchange.connect(_on_hour_change.unbind(1))
+	GlobalEventBus.sg_worldtime_change.connect(_on_time_change.unbind(1))
 	return
 
 func _on_hour_change() -> void:
+	# Change CO2
+	# TODO - change humidity if needed
 	_change_co2_by(-5)
+	return
+
+func _on_time_change() -> void:
+	# TODO - update plant growth information
 	return
 
 func _change_humidity_by(amount : int) -> void:
